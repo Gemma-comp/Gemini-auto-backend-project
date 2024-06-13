d0cef11a5d6e670dd914c30f1ffa4237737ff720
Elijah Darkeh <elijahagbedam18@gmail.com>
2024-06-13 07:50:45 -0700

    implement architecture

---
+++ Routes/ask_router.ts
@@ -0,0 +1,10 @@
+import controller from "controllers/ask_controller";
+import * as express from "express";
+
+const askRouter = express.Router();
+
+askRouter.post("/", controller);
+
+
+
+export default askRouter;
---
+++ controllers/ask_controller.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+
+const controller = (req: Request, res: Response): any => {
+    const { query } = req.body;
+
+    if (!query) return res.status(400).json({
+        status: "error",
+        message: "Query is required",
+    })
+
+    //Pass query to ai service
+     
+    
+    
+    
+    //Return ai response
+
+}
+
+
+export default controller;
